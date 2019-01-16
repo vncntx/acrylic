9697175c7b9047452e800e5921131b9d6d2fc26d
Vincent Fiestada <vincentfiestada@hotmail.com>
2019-01-16 13:02:37 +0800

    test: Add snapshot test for Row

---
+++ test/components/layout/Row.test.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import { Row } from "../../../lib/acrylic";
+
+describe("<Row />", () => {
+  it("Should match the latest snapshots", () => {
+    expect(
+      <Row>
+        <div>Column 1</div>
+        <div>Column 2</div>
+        <div>Column 3</div>
+      </Row>
+    ).toMatchSnapshot();
+  });
+});
