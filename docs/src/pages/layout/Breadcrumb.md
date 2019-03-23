5118afc6ebc81247a046769ca925ad6c1720056e
Vincent Fiestada <vffiestada@gmail.com>
2019-03-24 00:54:38 +0800

    feat: implement Breadcrumb component. closes #3

---
+++ docs/src/pages/layout/Breadcrumb.md
@@ -0,0 +1,13 @@
+## Breadcrumb
+
+The **Breadcrumb** component shows the user's position within the app. The **Crumbs** are layed out horizontally.
+
+```jsx
+render(() => (
+	<Breadcrumb elevation={1}>
+		<Crumb><a href="#">Files</a></Crumb>
+		<Crumb><a href="#">Library</a></Crumb>
+		<Crumb>Recent</Crumb>
+	</Breadcrumb>
+))
+```
---
+++ lib/components/layout/Breadcrumb.tsx
@@ -0,0 +1,40 @@
+import * as React from "react";
+import classNames from "classnames";
+import ILayoutProps from "./ILayoutProps";
+import { ISectionProps } from "./Section";
+
+/**
+ * Breadcrumbs show a user's position in the app
+ * @param props
+ */
+export default function Breadcrumb(props: ISectionProps) {
+	const { classes, children, elevation, acrylic, ...otherProps } = props;
+
+	const effectiveClass = classNames([
+		"acr-breadcrumb",
+		elevation && elevation > 0 ? `acr-elevated-${elevation}` : null,
+		acrylic && "acr-acrylic",
+		classes
+	]);
+
+	return (
+		<nav className={effectiveClass} {...otherProps}>
+			<ul>{children}</ul>
+		</nav>
+	);
+}
+
+/**
+ * An item in a Breadcrumb trail
+ * @param props
+ */
+export function Crumb(props: ILayoutProps) {
+	const { children, classes, ...otherProps } = props;
+	const effectiveClass = classNames(
+		"acr-crumb",
+		typeof children === "string" && "acr-li-text-only",
+		classes
+	);
+
+	return <li className={effectiveClass}>{children}</li>;
+}
